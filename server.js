8578f184f183813cb8f6fc2cfe1c09fd79ca6366
SylviaRemington <sylviaremington@gmail.com>
2025-07-02 14:47:42 -0700

    notes on ternary statement for const port

--- server.js
+++ server.js
@@ -21,6 +21,19 @@ const morgan = require("morgan"); //MORGAN
 // Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : "3000"; //ternery statement
 
+/*
+process.env.what ?
+You probably noticed a new port variable we’re defining and using in the app.listen statement. This will come in handy when we’re deploying our applications and cannot know in advance which port the hosting service will use.
+The syntax commonly used to set this variable is a ternary statement, which is essentially a condensed if...else statement. You could replace it with the following:
+
+let port;
+if (process.env.PORT) {
+  port = process.env.PORT;
+} else {
+  port = 3000;
+}
+*/
+
 
 
 // Connecting Mongoose to MongoDB Database
@@ -33,11 +46,11 @@ mongoose.connection.on("connected", () => {
 
 
 // Middleware to parse URL-encoded data from forms
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })); // so we can use query strings and req.body
 // Middleware for using HTTP verbs such as PUT or DELETE
-app.use(methodOverride("_method"));
+app.use(methodOverride("_method")); // method override so I can do PUT and DELETE requests
 // Morgan for logging HTTP requests
-app.use(morgan('dev'));
+app.use(morgan('dev')); // morgan for logging
 
 
 
